700d60cc0fea8a3f61a5c89cdf75687b51506b35
BaHaMuT7 <raul.pardoz@gmail.com>
2020-05-10 14:12:46 -0400

    Limpieza de imports no usuados

--- GestionTareas/src/main/java/cl/bahamut/GestionTareas/controller/MappingController.java
+++ GestionTareas/src/main/java/cl/bahamut/GestionTareas/controller/MappingController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import cl.bahamut.GestionTareas.entity.Servicio;
 import cl.bahamut.GestionTareas.service.IServicioService;
