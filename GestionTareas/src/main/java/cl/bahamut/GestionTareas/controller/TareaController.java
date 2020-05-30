bcbab295c775dbeb45f21a52c62291c0bc382738
BaHaMuT7 <raul.pardoz@gmail.com>
2020-05-30 02:49:30 -0400

    Cambios preliminares R1

--- GestionTareas/src/main/java/cl/bahamut/GestionTareas/controller/TareaController.java
+++ GestionTareas/src/main/java/cl/bahamut/GestionTareas/controller/TareaController.java
@@ -54,14 +54,56 @@ public class TareaController {
 		
 		if (resultado) {
 			
-			atributos.addAttribute("mensaje", "successAgregarTarea");
+			atributos.addFlashAttribute("mensaje", "successAgregarTarea");
 			return "redirect:/tareas";
 			
 		} else {
-			atributos.addAttribute("mensaje", "dangerAgregarTarea");
+			atributos.addFlashAttribute("mensaje", "dangerAgregarTarea");
 			return "redirect:/tareas";			
 		}		
 	}
+	
+	
+	@PostMapping(value="/modTarea")
+	public String modificarTarea(@RequestParam(value="descripcion") String descripcion,
+							     @RequestParam(value="cuerpo") String cuerpo,
+							     @RequestParam(value="fecha") String fecha,
+							     @RequestParam(value="plazo") String plazo,
+							     @RequestParam(value="servicio") int servicio,
+							     @RequestParam(value="pedidaPor") String pedidaPor,
+							     @RequestParam(value="sendEjecutadaPor") String ejecutadaPor,
+							     @RequestParam(value="codigo") String codigo,
+							     @RequestParam(value="cumplida") boolean cumplida,
+							     HttpSession sesion, 
+							     RedirectAttributes atributos) {
+			
+		
+		Tarea tarea = new Tarea();
+		
+		tarea.setDescripcion(descripcion);
+		tarea.setCodigo(codigo);
+		tarea.setCuerpo(cuerpo);
+		tarea.setFecha(Utils.stringToDate(fecha));
+		tarea.setPlazo(Utils.stringToDate(plazo));
+		tarea.setServicio(new Servicio(servicio));
+		tarea.setPedidaPor(pedidaPor);
+		tarea.setEjecutadaPor(ejecutadaPor);
+		tarea.setCumplida(cumplida);
+		tarea.setUsuario((Usuario)sesion.getAttribute("usuarioActivo"));
+		tarea.setCodigo(codigo);
+		
+		boolean resultado = repo.guardar(tarea);
+		
+		if (resultado) {
+			
+			atributos.addFlashAttribute("mensaje", "successModificarTarea");
+			return "redirect:/tareas";
+			
+		} else {
+			atributos.addFlashAttribute("mensaje", "dangerModificarTarea");
+			return "redirect:/tareas";			
+		}		
+	}	
 	
 	@GetMapping(value="/explorar/{codigoTarea}")
 	public String explorarTarea(@PathVariable String codigoTarea, HttpSession sesion, RedirectAttributes atributos) {
